372891f5c4431dc36259f9c74de8747e1a342be4
Piero Aguilar Anticona <107908190+Piers18@users.noreply.github.com>
2026-02-08 17:15:06 -0500

    feat: Add inline button styles directly to CounterPageComponent.

--- src/app/pages/counter/counter-page.component.ts
+++ src/app/pages/counter/counter-page.component.ts
@@ -2,7 +2,14 @@ import { Component } from "@angular/core";
 
 
 @Component({
-    templateUrl: 'counter-page.component.html'
+    templateUrl: './counter-page.component.html',
+    styles:`
+    button {
+        padding: 5px;
+        margin: 5px 10px;
+        width: 75px;
+    }
+    `
 })
 export class CounterPageComponent {
     counter: number = 10;
